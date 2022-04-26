f0b9e1de5f75d8f1bb16becdfe06fc188883bf7f
Vikrant kumar <42002633+vikrant5432@users.noreply.github.com>
2022-04-26 22:44:07 +0530

    Add files via upload

--- src/app/ADMIN/admin_Login Page/admin.login.page.component.ts
+++ src/app/ADMIN/admin_Login Page/admin.login.page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { HotToastService } from '@ngneat/hot-toast';
+import { adminDetail } from 'src/app/_services/admin-detail.services';
 import { AuthenticationService } from 'src/app/_services/authentication.service';
 import { NoticeService } from 'src/app/_services/notification.service';
 
@@ -18,7 +19,8 @@ export class adminLoginPageComponent {
     private notice: NoticeService,
     private formbuilder: FormBuilder,
     private router: Router,
-    private toast: HotToastService
+    private toast: HotToastService,
+    private adminDetail: adminDetail
   ) {}
   ngOnInit() {
     this.loginForm = this.formbuilder.group({
@@ -38,19 +40,49 @@ export class adminLoginPageComponent {
       this.notice.invalidLogin();
     } else {
       const { usermail, userpassword } = this.loginForm.value;
-      this.authSer
-        .login(usermail, userpassword)
-        .pipe(
-          this.toast.observe({
-            success: 'logIn successfully!',
-            loading: 'Logging In....',
-            error: 'There was an Error',
-          })
-        )
-        .subscribe((res) => {
-          this.router.navigate(['/admin-dashboard', res.user.uid]);
-          console.log(res.user.email);
+
+      this.adminDetail.getAdmin().then((res) => {
+        // for (let doc of res.docs) {
+        //   console.log(doc.data());
+        // }
+        const res1 = Object.values(res.docs);
+        const admin = res1.find((a: any) => {
+          return usermail === a.data().usermail;
         });
+        if (admin) {
+          console.log(admin.data()['usermail']);
+          this.authSer
+            .login(usermail, userpassword)
+            .pipe(
+              this.toast.observe({
+                success: 'logIn successfully!',
+                loading: 'Logging In....',
+                error: 'There was an Error',
+              })
+            )
+            .subscribe((res) => {
+              this.router.navigate(['/admin-dashboard', res.user.uid]);
+            });
+        } else {
+          this.toast.error('Only Admin are authenticate!!');
+        }
+      });
+      // if (usermail !== 'vikrantkumar@gmail.com') {
+      //   this.notice.invalidLogin();
+      // } else {
+      //   this.authSer
+      //     .login(usermail, userpassword)
+      //     .pipe(
+      //       this.toast.observe({
+      //         success: 'logIn successfully!',
+      //         loading: 'Logging In....',
+      //         error: 'There was an Error',
+      //       })
+      //     )
+      //     .subscribe((res) => {
+      //       this.router.navigate(['/admin-dashboard', res.user.uid]);
+      //     });
+      // }
     }
   }
 }
